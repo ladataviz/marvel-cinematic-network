13ed2f3a32fbe33987bd378acbfc549b2131304a
Tristan Guillevin <tristan.guillevin@gmail.com>
2022-08-26 16:37:44 +0200

    fix howard and data phase3

--- src/routes/phase3/+page.svelte
+++ src/routes/phase3/+page.svelte
@@ -1,16 +1,15 @@
 <script>
     import * as d3 from 'd3';
-    import data from '$lib/phase3.js';
+    // import data from '$lib/phase3.js';
+    // import {size} from "$lib/ColorHandler.svelte"
     import MainSection from '$lib/MainSection.svelte'
-    import {size} from "$lib/ColorHandler.svelte"
- 
+    import nodes from '$lib/phase3Nodes.js';
+    import links from '$lib/phase3Links.js';
+
 
     let phase = 3
    let csv = './phase3.csv'
 
-
-  let links = data.links.map(d => Object.create(d));
-  let nodes = data.nodes.map(d => Object.create(d));  
 
   // **** Scale for bubble size
   let sizeScalePers = d3
@@ -24,23 +23,30 @@
     .domain([14, 66])
     .range([30, 60]);
 
-  let sizeLinks = links;
 
-  // **** Create Force simulation
-  let simulation = d3
-    .forceSimulation(nodes)
-    .force("charge", d3.forceManyBody().strength(-183).theta(0))
-    .force("link", d3.forceLink(links).id(d => d.id))
-    .force("collide", d3.forceCollide( 
-      d=> d.type == 'character' 
-      ? size(d.id, d.type,sizeScalePers,sizeScaleMovie,sizeLinks)+2
-        : size(d.id,d.type,sizeScalePers,sizeScaleMovie,sizeLinks)+20 ))
-    .force("x", d3.forceX().strength(0.10))
-    .force("y", d3.forceY().strength(0.17 ))
-    .alphaTarget(0);
+  // let links = data.links.map(d => Object.create(d));
+  // let nodes = data.nodes.map(d => Object.create(d));  
+
+  // // **** Create Force simulation
+  // let simulation = d3
+  //   .forceSimulation(nodes)
+  //   .force("charge", d3.forceManyBody().strength(-183).theta(0))
+  //   .force("link", d3.forceLink(links).id(d => d.id))
+  //   .force("collide", d3.forceCollide( 
+  //     d=> d.type == 'character' 
+  //     ? size(d.id, d.type,sizeScalePers,sizeScaleMovie,links)+2
+  //       : size(d.id,d.type,sizeScalePers,sizeScaleMovie,links)+20 ))
+  //   .force("x", d3.forceX().strength(0.10))
+  //   .force("y", d3.forceY().strength(0.17 ))
+  //   .alphaTarget(0);
+
+  //      simulation.tick(400)
+
+  //   console.log(JSON.stringify(nodes, ['x','y','type','id','movieList','name','actorName','characterName']))
+  //   console.log(JSON.stringify(links, ['index','source','target','x','y','id']))
 
 
   </script>
 
-  <MainSection {phase} {csv} {links} {nodes} {simulation} {sizeLinks} {sizeScaleMovie} {sizeScalePers} />
+  <MainSection {phase} {csv} {links} {nodes} {sizeScaleMovie} {sizeScalePers} />
 
